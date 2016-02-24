1ee1601bf164cf034837194079a0325bfdf0f602
Lukas van Driel <lukas@q42.nl>
2016-02-24 12:17:12 +0000

    i'm getting something...

--- beaconhunt.js
+++ beaconhunt.js
@@ -7,23 +7,24 @@ if (Meteor.isServer) {
       uuid: 'b9407f30-f5f8-466e-aff9-25556b57fe6d',
       major: 38008,
       minor: 37725,
-      name: "Kevin 38008.37725"
+      identifier: "Kevin 38008.37725"
     });
   }
 }
 
 if (Meteor.isCordova) {
   Meteor.startup(() => {
-    Beacons.find().forEach((myBeacon, i) => {
-      var reactiveBeaconRegion = new cordova.plugins.locationManager.BeaconRegion(
-        myBeacon.name, myBeacon.uuid, myBeacon.major, myBeacon.minor
-      );
-      Tracker.autorun(function () {
-        var beaconRegion = reactiveBeaconRegion.getBeaconRegion();
-        if (beaconRegion) {
-          Beacons.update({_id:myBeacon._id}, {$set: { found: beaconRegion }})
-          console.log('found in region:', beaconRegion.inRegion);
-        }
+    Tracker.autorun(function () {
+      Beacons.find().forEach((myBeacon, i) => {
+        console.log('Searching for beacon', myBeacon.identifier);
+        let reactiveBeaconRegion = new ReactiveBeaconRegion(_.pick(myBeacon, 'uuid', 'major', 'minor', 'identifier'));
+        Tracker.autorun(function () {
+          var beaconRegion = reactiveBeaconRegion.getBeaconRegion();
+          if (beaconRegion) {
+            Beacons.update({_id:myBeacon._id}, {$set: { found: beaconRegion }})
+            console.log('found in region:', beaconRegion.inRegion);
+          }
+        });
       });
     });
   });
